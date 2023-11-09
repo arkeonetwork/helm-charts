7b8b965e72098c5b868f42750e773b24d4d6ea22
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2023-11-09 04:18:17 +0100

    fix cosmos error

--- unchained/charts/cosmos/files/init.sh
+++ unchained/charts/cosmos/files/init.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-    set -e
+set -e
 
-    start() {
-      MONIKER=unchained \
-      CHAIN_JSON=https://raw.githubusercontent.com/cosmos/chain-registry/master/cosmoshub/chain.json \
-      P2P_POLKACHU=true \
-      run.sh gaiad start \
-        --rpc.laddr tcp://0.0.0.0:26657 \
-        --minimum-gas-prices 0uatom &
-      PID="$!"
-    }
+start() {
+  MONIKER=unchained \
+  CHAIN_JSON=https://raw.githubusercontent.com/cosmos/chain-registry/master/cosmoshub/chain.json \
+  P2P_POLKACHU=true \
+  POLKACHU_NETWORK=cosmos \
+  run.sh gaiad start \
+    --rpc.laddr tcp://0.0.0.0:26657 \
+    --minimum-gas-prices 0uatom &
+  PID="$!"
+}
 
-    stop() {
-      echo "Catching signal and sending to PID: $PID" && kill $PID
-      while $(kill -0 $PID 2>/dev/null); do sleep 1; done
-    }
+stop() {
+  echo "Catching signal and sending to PID: $PID" && kill $PID
+  while $(kill -0 $PID 2>/dev/null); do sleep 1; done
+}
 
-    trap 'stop' TERM INT
-    start
-    wait $PID
+trap 'stop' TERM INT
+start
+wait $PID
