2d1fef6c303f5d7c4cca5e3e17efc7d115990c3c
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2023-12-23 01:39:46 +0100

    fix optimism

--- unchained/charts/optimism/files/daemon-init.sh
+++ unchained/charts/optimism/files/daemon-init.sh
@@ -6,17 +6,16 @@ apk add bash curl jq aria2 tar zstd openssl
 
 DATA_DIR=/data
 CHAINDATA_DIR=$DATA_DIR/geth/chaindata
+dirName="mainnet-bedrock.tar.zst"
 EXPECTED_CHECKSUM="c17067b7bc39a6daa14f71d448c6fa0477834c3e68a25e96f26fe849c12a09bffe510e96f7eacdef19e93e3167d15250f807d252dd6f6f9053d0e4457c73d5fb mainnet-bedrock.tar.zst"
 
 #if [ -n "$SNAPSHOT" ] && [ ! -d "$CHAINDATA_DIR" ]; then
 #  wget -c $SNAPSHOT -O - | tar --zstd -xvf - -C $DATA_DIR
 #fi
 
-if [ -n "$SNAPSHOT" ] && [ ! -d "$CHAINDATA_DIR" ]; then
+if [ -n "$SNAPSHOT" ]; then
     echo "Restoring from snapshot"
-
-    dirName="mainnet-bedrock.tar.zst"
-
+    
     # Download and extract the snapshot
     if [[ ! -f $DATA_DIR/$dirName ]]; then
     aria2c -s4 -x4 -k100M $SNAPSHOT -o $DATA_DIR
