73fd295b3866ddc77d15590a3e4718bd33b25e62
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2023-12-29 07:53:03 +0100

    fix bnb

--- unchained/charts/bnbsmartchain/files/daemon-init.sh
+++ unchained/charts/bnbsmartchain/files/daemon-init.sh
@@ -34,7 +34,7 @@ EXPECTED_CHECKSUM=b8b13f93cba9bb5b4f62d1586a10ae3b9615a3975e129503ab8692dff698ba
 #  fi
 #fi
 
-if [[ -n $SNAPSHOT && ! -f "/data/end" ]]; then
+if [[ -n $SNAPSHOT && ! -f "/data/endingss" ]]; then
     echo "Restoring from snapshot"
 
     apk add zstd lz4
@@ -56,14 +56,14 @@ if [[ -n $SNAPSHOT && ! -f "/data/end" ]]; then
     echo "uncompressing..."
     zstd -cd $DATA_DIR/$file | tar -xvf - -C $DATA_DIR
     echo "$dirName uncompressed"
-    touch /data/ending
+    touch /data/endings
 
 
 
     # Move extracted data to $DATA_DIR/geth
     mv /data/$dirName/geth $DATA_DIR/geth
     echo "$dirName moved"
-    touch /data/end
+    touch /data/endingss
   
 fi
 
