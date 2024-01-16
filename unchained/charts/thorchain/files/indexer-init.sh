6d317b2305e5b83e7c3444a5ac89522308843327
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2024-01-16 02:00:32 +0100

    enable thorchain

--- unchained/charts/thorchain/files/indexer-init.sh
+++ unchained/charts/thorchain/files/indexer-init.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-    set -e
+set -e
 
-    start_service() {
-      ./midgard config.json &
-      PID="$!"
-    }
+start_service() {
+  ./midgard config.json &
+  PID="$!"
+}
 
-    stop_service() {
-      echo "Catching signal and sending to PID: $PID"
-      kill $PID
-      while $(kill -0 $PID 2>/dev/null); do
-        sleep 1
-      done
-    }
+stop_service() {
+  echo "Catching signal and sending to PID: $PID"
+  kill $PID
+  while $(kill -0 $PID 2>/dev/null); do
+    sleep 1
+  done
+}
 
-    trap 'stop_service' TERM INT
+trap 'stop_service' TERM INT
 
-    start_service
-    wait $PID
+start_service
+wait $PID
