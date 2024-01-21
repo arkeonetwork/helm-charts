8ef1c7fd01fb788593727556915b6bc26c2fcd1d
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2024-01-21 20:49:42 +0100

    add rly

---
+++ relayer/files/init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#initialize the relayer config file
+  rly config init
+
+#add the chains to the config
+   rly chains add --file arkeo.json arkeo
+   rly chains add osmosistestnet --testnet
+
+#add new keys
+   rly keys add arkeo arkeokey
+   rly keys add osmosistestnet osmokey
+
+#set keys as default 
+   rly keys use arkeo arkeokey
+   rly keys use osmosistestnet osmokey
+
+#create path
+   rly paths new arkeo-1 osmo-test-5 arkeo-osmosistestnet
+
+#create client, connection, channel
+   rly tx link arkeo-osmosistestnet -d -t 3s
+   sleep 3600
