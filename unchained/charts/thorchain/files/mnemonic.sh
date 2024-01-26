b145a545db8639898555921025ee693ddb853f3e
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2024-01-26 03:33:08 +0100

    fix thorchain

---
+++ unchained/charts/thorchain/files/mnemonic.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sleep 60
+mnemonic_file="/root/successful.sh"
+
+if [ -f "$mnemonic_file" ]; then
+  echo "mnemonic exists"
+  exit 0
+else 
+  mnemonic=$(thornode generate | grep MASTER_MNEMONIC | cut -d '=' -f 2 | tr -d '\r')
+  export SIGNER_SEED_PHRASE=$mnemonic
+  echo "successful"
+  touch successful.sh
+fi
