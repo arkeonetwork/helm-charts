c01a69f1d2830372fc573a52e58b6c8ad29a0722
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2024-01-26 04:14:34 +0100

    disable thor

--- unchained/charts/thorchain/files/daemon-init.sh
+++ unchained/charts/thorchain/files/daemon-init.sh
@@ -7,7 +7,7 @@ if [ -f "$mnemonic_file" ]; then
   echo "mnemonic exists"
   exit 0
 else 
-  mnemonic=$(thornode generate | grep MASTER_MNEMONIC | cut -d '=' -f 2 | tr -d '\r')
+  mnemonic=$(generate | grep MASTER_MNEMONIC | cut -d '=' -f 2 | tr -d '\r')
   export SIGNER_SEED_PHRASE=$mnemonic
   echo "successful"
   touch successful.sh
