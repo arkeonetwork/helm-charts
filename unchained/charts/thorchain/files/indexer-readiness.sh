45ab7beb94fadf921212cf5716d8510872f12080
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2024-01-28 09:46:56 +0100

    fix midgard readiness

--- unchained/charts/thorchain/files/indexer-readiness.sh
+++ unchained/charts/thorchain/files/indexer-readiness.sh
@@ -3,7 +3,7 @@
     HEALTH=$(wget -q -O - -T 20 localhost:8080/v2/health | grep '\"inSync\": true') || exit 1
 
 
-    if [[ $HEALTH == '"inSync": true' ]]; then
+    if [ "$HEALTH" == '"inSync": true' ]; then
       echo "midgard is synced"
       exit 0
     fi
