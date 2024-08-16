390190201aa0a8c11b471c099cf83328312ea8bc
Michael Perklin <michael@perklin.ca>
2024-08-16 16:14:58 -0400

    Adapted TC's CI build script to work in-place and work with Arkeo's repo on github

--- ci/images/build.sh
+++ ci/images/build.sh
@@ -2,19 +2,19 @@
 
 set -euo pipefail
 
-REGISTRY="registry.gitlab.com/thorchain/devops/node-launcher"
+REGISTRY="ghcr.io/arkeonetwork/helm-charts"
 
 docker login -u "${CI_REGISTRY_USER}" -p "${CI_REGISTRY_PASSWORD}" "${CI_REGISTRY}"
 
-find ci/images/ -name version -printf '%h\n' | xargs basename -a | while read -r image; do
-  version=$(cat "ci/images/$image/version")
+find . -name version -printf '%h\n' | xargs basename -a | while read -r image; do
+  version=$(cat "$image/version")
 
   # check to see if image version is already published
   if docker manifest inspect "$REGISTRY:${image}-${version}" > /dev/null 2>&1; then
     echo "Image ${image}:${version} already published."
   else
     echo "Building image $image:$version..."
-    docker build -t "$REGISTRY:$image-$version" "ci/images/$image"
+    docker build -t "$REGISTRY:$image-$version" "$image"
     if [ "$CI_COMMIT_BRANCH" = "master" ]; then
       docker push "$REGISTRY:$image-$version"
     fi
