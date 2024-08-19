a236861a78d3e722356194cf8299b804f2aaf727
Michael Perklin <michael@perklin.ca>
2024-08-18 21:11:22 -0400

    Updated build system to build amd64 architecture

--- ci/images/build.sh
+++ ci/images/build.sh
@@ -6,15 +6,15 @@ REGISTRY="ghcr.io/arkeonetwork/helm-charts"
 
 docker login -u "${CI_REGISTRY_USER}" -p "${CI_REGISTRY_PASSWORD}" "${CI_REGISTRY}"
 
-find . -name version -printf '%h\n' | xargs basename -a | while read -r image; do
-  version=$(cat "$image/version")
+find ci/images/ -name version -printf '%h\n' | xargs basename -a | while read -r image; do
+  version=$(cat "ci/images/$image/version")
 
   # check to see if image version is already published
   if docker manifest inspect "$REGISTRY:${image}-${version}" > /dev/null 2>&1; then
     echo "Image ${image}:${version} already published."
   else
     echo "Building image $image:$version..."
-    docker build -t "$REGISTRY:$image-$version" "$image"
+    docker build --platform linux/amd64 -t "$REGISTRY:$image-$version" "ci/images/$image"
     if [ "$CI_COMMIT_BRANCH" = "master" ]; then
       docker push "$REGISTRY:$image-$version"
     fi
