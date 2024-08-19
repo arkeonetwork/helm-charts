2efb3a16e214d4b62a86013d82ada0e8f06257a5
Michael Perklin <michael@perklin.ca>
2024-08-18 21:41:02 -0400

    Fixed bug where an empty priv_validator_state.json was not written before first launch

--- ci/images/osmosis-daemon/entrypoint.sh
+++ ci/images/osmosis-daemon/entrypoint.sh
@@ -2,8 +2,10 @@
 
 # initialize config
 if [ ! -f "/root/.osmosisd/config/app.toml" ]; then
+  echo "Writing initial config"
   mkdir -p /root/.osmosisd/config
   cp /etc/osmosis/app.toml /root/.osmosisd/config/app.toml
+  echo '{"height":"0","round":0,"step":0}' > /root/.osmosisd/data/priv_validator_state.json
 fi
 
 exec /osmosisd start --log_format json --rpc.laddr tcp://0.0.0.0:26657 --x-crisis-skip-assert-invariants "$@"
